78175bdf5987078ac7080df21a83acb5edbf0065
unknown <arfinpeul523@gmail.com>
2023-12-03 03:35:15 +0600

    data reserved

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    app.post("/newproduct", async (req, res) => {
+      const newproduct = req.body;
+      console.log(newproduct);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +36,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
