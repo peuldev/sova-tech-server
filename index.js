f61d1b3c43ae8d42bca94614f52f2fffbba245c6
unknown <arfinpeul523@gmail.com>
2023-12-03 18:34:10 +0600

    ser error fiexd

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 var cors = require("cors");
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_PRODUCT}:${process.env.SECRET_KEY}@sova.04n0q2g.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+const uri =
+  "mongodb+srv://arfinpeul523:n2KuNq79J6tIy6g7@sova.04n0q2g.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
