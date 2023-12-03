2437777603e43da96439b6d84d211c896726a4b5
unknown <arfinpeul523@gmail.com>
2023-12-03 12:33:38 +0600

    set id

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 var cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,6 +25,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const productcollection = client.db("productDB").collection("product");
+
+    app.get("/newproduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productcollection.findOne(query);
+      res.send(result);
+    });
 
     app.get("/newproduct", async (req, res) => {
       const cursor = productcollection.find();
